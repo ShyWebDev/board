afaa44d2bc61280819750caa1d9a6bc03aceb58f
wstlits <c1216sg@hanmai..net>
2021-05-27 18:00:51 +0900

    first comming

---
+++ src/main/java/com/ssungis/board/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.ssungis.board.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@Api("board Test API")
+@RequestMapping(path="/api/v1/test")
+@RestController
+public class TestController {
+
+    @GetMapping("")
+    @ApiOperation(value = "board Test API")
+    public String getTest(){
+        return "board";
+    }
+}
