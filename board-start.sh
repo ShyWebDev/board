922f3002c600321be2663851fc55f09bbdab71c6
wstlits <c1216sg@hanmai..net>
2021-06-08 16:21:37 +0900

    배포 테스트

--- board-start.sh
+++ board-start.sh
@@ -3,16 +3,15 @@
 REPOSITORY=/home/ssungis/apiGateway/board
 cd $REPOSITORY
 
-
 JAR_NAME=$(ls $REPOSITORY/| grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/$JAR_NAME
-APP_NAME=$JAR_NAME
+
 
 echo "> JAR_NAME $JAR_NAME"
 echo "> JAR_PATH $JAR_PATH"
 echo "> APP_NAME $APP_NAME"
 
-CURRENT_PID=$(pgrep -f $APP_NAME)
+CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
