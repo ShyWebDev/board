6df4755eb696489a871bebfc83a408fb537413ef
wstlits <c1216sg@hanmai..net>
2021-06-08 15:29:30 +0900

    배포 테스트

--- board-start.sh
+++ board-start.sh
@@ -7,5 +7,16 @@ APP_NAME=board
 JAR_NAME=$(ls $REPOSITORY/| grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/$JAR_NAME
 
+CURRENT_PID=$(pgrep -f $APP_NAME)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> 종료할것 없음."
+else
+  echo "> kill -9 $CURRENT_PID"
+  kill -9 $CURRENT_PID
+  sleep 5
+fi
+
 echo "> $JAR_PATH 배포"
 nohup java -jar $JAR_PATH &
