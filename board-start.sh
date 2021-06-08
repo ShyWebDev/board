8dd0284cb9d5582bdf6ff23874b69fd0ac22695f
wstlits <c1216sg@hanmai..net>
2021-06-08 12:26:24 +0900

    배포관련 수정

--- board-start.sh
+++ board-start.sh
@@ -3,20 +3,9 @@
 REPOSITORY=/home/ssungis/apiGateway/board
 cd $REPOSITORY
 
-APP_NAME=airbnb
+APP_NAME=board
 JAR_NAME=$(ls $REPOSITORY/| grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/$JAR_NAME
-
-CURRENT_PID=$(pgrep -f $APP_NAME)
-
-if [ -z $CURRENT_PID ]
-then
-  echo "> 종료할것 없음."
-else
-  echo "> kill -9 $CURRENT_PID"
-  kill -9 $CURRENT_PID
-  sleep 5
-fi
 
 echo "> $JAR_PATH 배포"
 nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
