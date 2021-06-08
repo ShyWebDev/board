a2c4c07b864c3c42b4084ef9110e4d56242d3012
wstlits <c1216sg@hanmai..net>
2021-06-08 12:27:45 +0900

    배포관련 수정

--- board-start.sh
+++ board-start.sh
@@ -9,11 +9,3 @@ JAR_PATH=$REPOSITORY/$JAR_NAME
 
 echo "> $JAR_PATH 배포"
 nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
-#!/usr/bin/env bash
-
-REPOSITORY=/home/ssungis/apiGateway/board
-cd $REPOSITORY
-
-APP_NAME=airbnb
-JAR_NAME=$(ls $REPOSITORY/| grep '.jar' | tail -n 1)
-JAR_PATH=$REPOSITORY/$JAR_NAME
